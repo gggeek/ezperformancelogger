31d48b1992030645e39f24883ebb868c7a3ddd42
gg <gg@c13450e9-e943-46af-aba4-8f01ea4cd9f9>
2010-11-04 20:14:06 +0000

    - some more comments
    git-svn-id: http://svn.projects.ez.no/ezperformancelogger/trunk@4 c13450e9-e943-46af-aba4-8f01ea4cd9f9

--- extension/ezperforrmancelogger/classes/ezperflogger.php
+++ extension/ezperforrmancelogger/classes/ezperflogger.php
@@ -3,8 +3,9 @@
 * An 'output filter' class that does not filter anything, but passes some info
 * to apache that can log it in the access logs
 *
-* @todo add logging of nr. of queries
+* @todo add some ini setting deciding what to log
 * @todo log total elapsed time (interesting to compare to Apache's own time measure)
+* @todo log total cluster queries (see code in ezdebug extension)
 * @todo add an ini file with the name of file to log to ('apache' for apache notes)
 */
 class ezPerfLogger
@@ -12,6 +13,20 @@ class ezPerfLogger
     static function filter( $output )
     {
         apache_note( 'mem_usage', memory_get_peak_usage( true ) );
+
+        /* add number of db queries too (nb: only works when debug is enabled? */
+        /*$ini = eZINI::instance();
+        // we cannot use $db->databasename() because we get the same for mysql and mysqli
+        $type = preg_replace( '/^ez/', '', $ini->variable( 'DatabaseSettings', 'DatabaseImplementation' ) );
+        $type .= '_query';
+        // read accumulator
+        $debug = eZDebug::instance();
+        if ( isset( $debug->TimeAccumulatorList[$type] ) )
+        {
+            $num = $debug->TimeAccumulatorList[$type]['count'];
+            apache_note( 'num_queries', $num );
+        }*/
+
         return $output;
     }
 }
