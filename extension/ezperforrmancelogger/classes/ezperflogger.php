dc9a123367578531ddfe0c444b861b06f3cecb53
gg <gg@c13450e9-e943-46af-aba4-8f01ea4cd9f9>
2011-09-30 23:26:33 +0000

    - add suopport for piwik, google-analytics custom variables - round mem usage to 1000 bytes, time to 1 msec
    git-svn-id: http://svn.projects.ez.no/ezperformancelogger/trunk@5 c13450e9-e943-46af-aba4-8f01ea4cd9f9

--- extension/ezperforrmancelogger/classes/ezperflogger.php
+++ extension/ezperforrmancelogger/classes/ezperflogger.php
@@ -1,10 +1,8 @@
 <?php
 /**
-* An 'output filter' class that does not filter anything, but passes some info
-* to apache that can log it in the access logs
+* An 'output filter' class that does not filter anything, but logs some perf values
+* to different "log" types
 *
-* @todo add some ini setting deciding what to log
-* @todo log total elapsed time (interesting to compare to Apache's own time measure)
 * @todo log total cluster queries (see code in ezdebug extension)
 * @todo add an ini file with the name of file to log to ('apache' for apache notes)
 */
@@ -12,20 +10,84 @@ class ezPerfLogger
 {
     static function filter( $output )
     {
-        apache_note( 'mem_usage', memory_get_peak_usage( true ) );
+        global $scriptStartTime;
 
-        /* add number of db queries too (nb: only works when debug is enabled? */
-        /*$ini = eZINI::instance();
-        // we cannot use $db->databasename() because we get the same for mysql and mysqli
-        $type = preg_replace( '/^ez/', '', $ini->variable( 'DatabaseSettings', 'DatabaseImplementation' ) );
-        $type .= '_query';
-        // read accumulator
-        $debug = eZDebug::instance();
-        if ( isset( $debug->TimeAccumulatorList[$type] ) )
+        $ini = eZINI::instance( 'ezperformancelogger.ini' );
+
+        $values = array();
+        foreach( $ini->variable( 'GeneralSettings', 'TrackVariables' ) as $i => $var )
         {
-            $num = $debug->TimeAccumulatorList[$type]['count'];
-            apache_note( 'num_queries', $num );
-        }*/
+            switch( $var )
+            {
+                case 'mem_usage':
+                    $values[$i] = round( memory_get_peak_usage( true ), -3 );
+                    break;
+                case 'execution_time':
+                    $values[$i] = round( microtime( true ) - $scriptStartTime, 3 ); /// @todo test if $scriptStartTime is set
+                    break;
+                case 'db_queries':
+                    // (nb: only works when debug is enabled?)
+                    $dbini = eZINI::instance();
+                    // we cannot use $db->databasename() because we get the same for mysql and mysqli
+                    $type = preg_replace( '/^ez/', '', $dbini->variable( 'DatabaseSettings', 'DatabaseImplementation' ) );
+                    $type .= '_query';
+                    //die($type);
+                    // read accumulator
+                    $debug = eZDebug::instance();
+                    if ( isset( $debug->TimeAccumulatorList[$type] ) )
+                    {
+                        $values[$i] = $debug->TimeAccumulatorList[$type]['count'];
+                    }
+                    else
+                    {
+                        $values[$i] = "0"; // can not tell between 0 reqs per page and no debug...
+                    }
+                    break;
+            }
+        }
+
+        foreach( $ini->variable( 'GeneralSettings', 'LogMethods' ) as $method )
+        {
+            switch( $method )
+            {
+                case 'apache':
+                    foreach( $ini->variable( 'GeneralSettings', 'TrackVariables' ) as $i => $var )
+                    {
+                        apache_note( $var, $values[$i] );
+                    }
+                    break;
+
+                case 'piwik':
+                    $text = '';
+                    foreach( $ini->variable( 'GeneralSettings', 'TrackVariables' ) as $i => $var )
+                    {
+                        $text .= "\npiwikTracker.setCustomVariable( $i, \"$var\", \"{$values[$i]}\", \"page\" );";
+                    }
+                    $text .= "\npiwikTracker.trackPageView();";
+                    //die('hi');
+                    $output = preg_replace( '/piwikTracker\.trackPageView\( *\);?/', $text, $output );
+                    break;
+
+                case 'googleanalytics':
+                    $text = '';
+                    foreach( $ini->variable( 'GeneralSettings', 'TrackVariables' ) as $i => $var )
+                    {
+                        $text .= "\n_gaq.push([$i, '$var', '{$values[$i]}', 3]);";
+                    }
+                    $text .= "\n_gaq.push(['_trackPageview']);";
+                    $output = preg_replace( "/_gaq.push\( *[ *['\"]_trackPageview['\"] *] *\);?/", $text, $output );
+                    break;
+
+                /// @todo
+                /*case 'logfile':
+                    ;
+                    break;
+                case 'database':
+                    ;
+                    break;*/
+
+            }
+        }
 
         return $output;
     }
--- extension/ezperforrmancelogger/ezinfo.php
+++ extension/ezperforrmancelogger/ezinfo.php
@@ -4,8 +4,8 @@ class ezapacheperflogInfo
     static function info()
     {
         return array( 'Name' => "<a href=\"http://projects.ez.no/ezperformancelogger\">ezperformancelogger</a>",
-                      'Version' => "0.1-dev",
-                      'Copyright' => "Copyright (C) 2010 Gaetano Giunta",
+                      'Version' => "0.2-dev",
+                      'Copyright' => "Copyright (C) 2010-2011 Gaetano Giunta",
                       'License' => "GNU General Public License v2.0"
                      );
     }
