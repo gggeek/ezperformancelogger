1b31552fce7d37007065d5925fb319b2a97653bc
gggeek <gg@ez.no>
2012-04-17 18:22:32 +0200

    Prepare tags for post-0.3

--- ezinfo.php
+++ ezinfo.php
@@ -4,7 +4,7 @@ class eZperformanceloggerInfo
     static function info()
     {
         return array( 'Name' => "<a href=\"http://projects.ez.no/ezperformancelogger\">ezperformancelogger</a>",
-                      'Version' => "0.3-dev",
+                      'Version' => "0.4-dev",
                       'Copyright' => "Copyright (C) 2010-2012 Gaetano Giunta",
                       'License' => "GNU General Public License v2.0"
                      );
