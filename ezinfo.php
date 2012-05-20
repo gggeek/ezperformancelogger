f53d1f207bfd93e2951ab0bc99f2b67ab89db9a5
gggeek <gg@ez.no>
2012-05-20 17:23:09 +0200

    Update version info Update todo

--- ezinfo.php
+++ ezinfo.php
@@ -4,7 +4,7 @@ class eZperformanceloggerInfo
     static function info()
     {
         return array( 'Name' => "<a href=\"http://projects.ez.no/ezperformancelogger\">ezperformancelogger</a>",
-                      'Version' => "0.5-dev",
+                      'Version' => "0.6-dev",
                       'Copyright' => "Copyright (C) 2010-2012 Gaetano Giunta",
                       'License' => "GNU General Public License v2.0",
                       '3rdparty_software' =>
