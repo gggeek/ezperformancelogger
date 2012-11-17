41221656f0defa1b3f3867f6c42aa7bff326630c
gggeek <gg@ez.no>
2012-11-17 14:02:49 +0100

    Improve docs

--- doc/sample_config.php
+++ doc/sample_config.php
@@ -1,5 +1,10 @@
 <?php
 
+// This is a sample of php code which can be added at the start of your
+// config.php file to activate profiling with XHPROF.
+// Profiling of a web page will only be triggered when the url used to acces it
+// contains in the query string ?STARTXHPROF=1
+
 if ( isset( $_GET['STARTXHPROF'] ) )
 {
     /// NB: php class autoloading is not yet set up at this point
