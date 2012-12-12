82e1a480098e8b46ea04b6ee0794bdb239dc213d
gggeek <gg@ez.no>
2012-12-12 17:08:45 +0100

    Add tracer classes for eZP 5.0 LS

---
+++ classes/tracers/5.0/tracingdfs.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Extends the base dfs backend to add tracing points
+ */
+
+class eZDFSFileHandlerTracing50DFSBackend extends eZDFSFileHandlerDFSBackend
+{
+
+    /**
+     * Overrides methods from parent to always trace data even when debug is off
+     */
+    protected function accumulatorStart()
+    {
+        eZPerfLogger::accumulatorStart( 'mysql_cluster_dfs_operations', 'MySQL Cluster', 'DFS operations' );
+    }
+
+    /**
+     * Overrides methods from parent to always trace data even when debug is off
+     */
+    protected function accumulatorStop()
+    {
+        eZPerfLogger::accumulatorStop( 'mysql_cluster_dfs_operations' );
+    }
+}
+?>
