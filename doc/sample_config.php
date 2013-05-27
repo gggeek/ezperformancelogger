847759628f821e0cecab04aca6c266fff75ce45d
gggeek <gg@ez.no>
2013-05-27 17:54:35 +0200

    more docs on xhprof

--- doc/sample_config.php
+++ doc/sample_config.php
@@ -4,6 +4,12 @@
 // config.php file to activate profiling with XHPROF.
 // Profiling of a web page will only be triggered when the url used to acces it
 // contains in the query string ?STARTXHPROF=1
+//
+// NOTE: maybe it is not a good idea to use this code in production: you would be giving your end users the possibility to slow down
+// your site and fill your hard disk just by using a "hacked" url.
+//
+// Another useful workflow could be to enable profiling on 1 page every 100, using a random-variable test.
+// This way you would be able to get statistically useful data from real-life users while avoiding slowing down every sigle page
 
 if ( isset( $_GET['STARTXHPROF'] ) )
 {
