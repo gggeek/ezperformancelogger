6105e873da68e9c3336fb2f2b2dc61e9c1e244f0
gggeek <gg@ez.no>
2014-09-18 11:22:58 +0200

    Tag version

--- ezinfo.php
+++ ezinfo.php
@@ -4,7 +4,7 @@ class eZperformanceloggerInfo
     static function info()
     {
         return array( 'Name' => "<a href=\"http://projects.ez.no/ezperformancelogger\">ezperformancelogger</a>",
-                      'Version' => "0.12-dev",
+                      'Version' => "0.12.0",
                       'Copyright' => "Copyright (C) 2010-2014 eZ Systems AS",
                       'License' => "GNU General Public License v2.0",
                       '3rdparty_software' =>
